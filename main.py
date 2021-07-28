93b224cef9ddb0f1b403a1e8a57d1ba476e6c090
klocke0 <79632221+klocke0@users.noreply.github.com>
2021-07-27 22:30:41 -0700

    Added drawing sudoku box lines and started board class

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pygame
 
-WIDTH, HEIGHT = 900, 500
+WIDTH, HEIGHT = 600, 600
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Sudoku Solver")
 
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 
 def main():
@@ -22,6 +23,14 @@ def main():
 
 def draw_window():
     WINDOW.fill(WHITE)
+    # Draws lines of sudoku board
+    for i in range(0, 541, 180):
+        pygame.draw.line(WINDOW, BLACK, (i + 30, 0 + 30), (i + 30, 540 + 30), 4)
+        pygame.draw.line(WINDOW, BLACK, (i + 30 + 60, 0 + 30), (i + 30 + 60, 540 + 30), 2)
+        pygame.draw.line(WINDOW, BLACK, (i + 30 + 120, 0 + 30), (i + 30 + 120, 540 + 30), 2)
+        pygame.draw.line(WINDOW, BLACK, (0 + 30, i + 30), (540 + 30, i + 30), 4)
+        pygame.draw.line(WINDOW, BLACK, (0 + 30, i + 30 + 60), (540 + 30, i + 30 + 60), 2)
+        pygame.draw.line(WINDOW, BLACK, (0 + 30, i + 30 + 120), (540 + 30, i + 30 + 120), 2)
     pygame.display.update()
 
 if __name__ == "__main__":
