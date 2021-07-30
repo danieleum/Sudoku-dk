f58549be425ce6e9d6b8e8c9b7f81f01cb446d82
danieleum <daniel42eum@gmail.com>
2021-07-29 19:59:34 -0700

    Update main.py
    created the initial grid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 
-WIDTH, HEIGHT = 900, 500
+WIDTH, HEIGHT = 800, 700
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Sudoku Solver")
 
@@ -23,6 +23,13 @@ def main():
 
 def draw_window():
     WINDOW.fill(WHITE)
+    for i in range(0, 10):
+        if i % 3 == 0:
+            pygame.draw.line(WINDOW, (0,0,0), (50 + 50 * i, 50), (50 + 50 * i, 500), 4) 
+            pygame.draw.line(WINDOW, (0,0,0), (50, 50 + 50 * i), (500, 50 + 50 * i), 4)
+
+        pygame.draw.line(WINDOW, (0,0,0), (50 + 50 * i, 50), (50 + 50 * i, 500), 2) 
+        pygame.draw.line(WINDOW, (0,0,0), (50, 50 + 50 * i), (500, 50 + 50 * i), 2)
     pygame.display.update()
 
 if __name__ == "__main__":
